bf85c4b2c82605cc9147d33667d6f059e9438b6b
NicolayF <nicolayfro@gmail.com>
2022-12-29 11:44:49 -0300

    day4

---
+++ day4/part1.py
@@ -0,0 +1,19 @@
+#spli multiple delimiters
+import re
+
+#Get data
+with open("input.in") as f:
+    data = [i for i in f.read().strip().split("\n")]
+
+sum = 0
+for i in data:
+    arr = re.split(r'-|,', i)
+    elf1Start = int(arr[0])
+    elf1End = int(arr[1])
+    elf2Start = int(arr[2])
+    elf2End = int(arr[3])
+    if ((elf1Start >= elf2Start) & (elf1End <= elf2End)) \
+    | ((elf1Start <= elf2Start) & (elf1End >= elf2End)):
+        sum += 1
+
+print(sum)
