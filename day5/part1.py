9d9ac8d68f712faf460d5bcbf1e5c3d7e7a961d5
NicolayF <nicolayfro@gmail.com>
2022-12-30 12:05:55 -0300

    day5 part1

---
+++ day5/part1.py
@@ -0,0 +1,34 @@
+with open("input.in") as f:
+    crateString, instructions = (i.splitlines() for i in f.read().strip("\n").split("\n\n"))
+
+inputStacks = [crate.replace("    ","[0]").replace(" ","").replace("[","") for crate in crateString[:-1]]
+inputStacks = [crate.split("]")[:-1] for crate in inputStacks]
+stacksTotal = int(crateString[-1].replace(" ","")[-1])
+
+#Initial stacks
+stacks = [[] for i in range(stacksTotal)]
+for i in range(len(inputStacks)):
+    for j in range(stacksTotal):
+        if inputStacks[i][j] != '0':
+            stacks[j].insert(0, inputStacks[i][j])
+
+#Clean instructions string into:
+#number of crates to move, from what pile to move and where.
+for instruction in instructions:
+    instruction = instruction.replace("move ","").replace("from ","").replace("to ","").strip().split(" ")
+    instruction = [int(i) for i in instruction]
+    numberOfCrates = instruction[0]
+    fromPile = instruction[1]
+    toPile = instruction[2]
+    for i in range(numberOfCrates):
+        stacks[toPile-1].append(stacks[fromPile-1][-1])
+        stacks[fromPile-1].pop()
+
+
+def showStacks():
+    print("Stacks:\n")
+    for i in range(len(stacks)):
+        print(i, stacks[i])
+    print("\n")
+
+showStacks()
