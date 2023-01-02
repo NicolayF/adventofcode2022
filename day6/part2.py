76f94de7be1eb101421b2f869024e049cf312ce3
NicolayF <nicolayfro@gmail.com>
2023-01-02 07:25:01 -0300

    day6 part2 extra

--- day6/part2.py
+++ day6/part2.py
@@ -7,5 +7,14 @@ def uniqueStrings(str):
     for i in range(len(str)):
         if i >= 14 and len(set([str[i-j] for j in range(14)])) == 14:
             return i+1
-        
+
 print(uniqueStrings(stringInput))
+
+#Another way to resolve part 1 and 2.
+def solve(k):
+    for i in range(len(stringInput)):
+        if len(set(stringInput[i:i+k]))==k:
+            return i+k
+
+print(solve(4))
+print(solve(14))
