c2aaaf28380c2219d92d27b63190384074bd860c
NicolayF <nicolayfro@gmail.com>
2023-01-02 07:10:16 -0300

    day6 part2

--- day6/part2.py
+++ day6/part2.py
@@ -1,3 +1,11 @@
 #Get data
 with open("input.in") as f:
     stringInput = f.read()
+    
+#Sets can only have unique elements
+def uniqueStrings(str):
+    for i in range(len(str)):
+        if i >= 14 and len(set([str[i-j] for j in range(14)])) == 14:
+            return i+1
+        
+print(uniqueStrings(stringInput))
